3a0f130182a9ce9bdd0a9b9d811e1a17e476dc2f
Rushikesh More <102665694+RushikeshMore11@users.noreply.github.com>
2024-02-15 10:16:13 +0530

    feat: Firebase set up (#2)
    * feat: Firebase set up
    
    Firebase setup
    API for firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { signIn, signInWithGoogle, signUp } from "./firebase/auth";
 
 const App = () => {
-  return <div>App</div>;
+  const handleSignIn = async () => {
+    try {
+      const data = await signIn("abc@gmail.com", "Ruta@2028");
+      console.log(data);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const data = await signUp("abc@gmail.com", "Ruta@2028");
+      console.log(data);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const handleSignUpWithGoogle = async () => {
+    try {
+      const data = await signInWithGoogle();
+      console.log(data);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={handleSignIn}>Sign In</button>
+      <button onClick={handleSignUp}>Sign Up</button>
+      <button onClick={handleSignUpWithGoogle}>Google</button>
+    </div>
+  );
 };
 
 export default App;
